260c6aa1b231b7f942fd40b4f86f997b55520347
Daisuke Toyoda <toyody0420@gmail.com>
2019-10-16 16:35:58 +0900

    made task creator

--- config/database.php
+++ config/database.php
@@ -35,6 +35,12 @@ return [
 
     'connections' => [
 
+        'sqlite_testing' => [
+            'driver' => 'sqlite',
+            'database' => ':meory:',
+            'prefix' => '',
+        ],
+
         'sqlite' => [
             'driver' => 'sqlite',
             'url' => env('DATABASE_URL'),
